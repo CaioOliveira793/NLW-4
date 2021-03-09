75e2499c95dab570deccc9c863cd1ed23af8aedd
Caio Oliveira <caio.vsoliveira23@gmail.com>
2021-03-09 14:30:27 -0300

    fix: list surveys query

--- src/useCases/listSurveys/ListSurveysUseCase.ts
+++ src/useCases/listSurveys/ListSurveysUseCase.ts
@@ -1,7 +1,7 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { Repository, ILike } from "typeorm";
-import { providers } from "../../constants";
-import { Survey } from "../../entities/Survey.entity";
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository, ILike, ObjectLiteral } from 'typeorm';
+import { providers } from '../../constants';
+import { Survey } from '../../entities/Survey.entity';
 
 export interface ListSurveysRequestDTO {
 	title?: string,
@@ -18,12 +18,16 @@ export class ListSurveysUseCase {
 	) {}
 
 	public async execute(data: ListSurveysRequestDTO): Promise<Survey[]> {
-		const skip = Math.max(data.page ?? 1, 1);
+		const skip = ((data.page ?? 1) - 1) * 20;
+
+		const props = {} as ObjectLiteral;
+		if (data.title)
+			props.title = ILike(`%${data.title}%`);
+		if (data.description)
+			props.description = ILike(`%${data.description}%`);
+
 		return await this.surveyRepository.find({
-			where: {
-				title: (data.title) ? ILike(`%${data.title}%`) : undefined,
-				description: (data.description) ? ILike(`%${data.description}%`) : undefined
-			},
+			where: props,
 			take: 20,
 			skip
 		});

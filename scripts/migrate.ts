acf3acefa0e7a446eb65c0190f298c437ae3dac1
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2021-05-23 12:41:46 -0300

    chore: add migrate script

---
+++ scripts/migrate.ts
@@ -0,0 +1,12 @@
+import { createConnection } from 'typeorm';
+
+import connectionOptions from '../src/database/connections';
+
+async function migrate(): Promise<void> {
+	const connection = await createConnection(connectionOptions);
+	await connection.connect();
+	await connection.runMigrations();
+	await connection.close();
+}
+
+migrate();
